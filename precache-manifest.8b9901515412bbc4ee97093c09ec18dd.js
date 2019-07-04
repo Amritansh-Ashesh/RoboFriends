641a90654ab7847ab044ef12efa92e6af58f3a1c
Ted <51405088+Amritansh-Ashesh@users.noreply.github.com>
2019-07-04 14:15:11 +0530

    Added Build Version

---
+++ precache-manifest.8b9901515412bbc4ee97093c09ec18dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727e97948f1618f2a3414ee33b4cdc9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "209b17247558a4098ea5",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "40de12089a04f28e1292",
+    "url": "/static/css/main.e20491ec.chunk.css"
+  },
+  {
+    "revision": "209b17247558a4098ea5",
+    "url": "/static/js/2.29f1c20e.chunk.js"
+  },
+  {
+    "revision": "40de12089a04f28e1292",
+    "url": "/static/js/main.e9480497.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
