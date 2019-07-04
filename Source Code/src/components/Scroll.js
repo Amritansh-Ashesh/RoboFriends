79a86ad769a666eaade6dda209bc2f9fb4376992
Ted <51405088+Amritansh-Ashesh@users.noreply.github.com>
2019-07-04 19:15:15 +0530

    Removed Scroll

--- Source Code/src/components/Scroll.js
+++ Source Code/src/components/Scroll.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Scroll = (props) => {
     return (
-        <div style={{overflowY : 'scroll', padding: '2rem 5rem'}}>
+        <div style={{overflowY : 'hidden', padding: '2rem 5rem'}}>
             {props.children}
         </div>
     );
 }
 
-export default Scroll;
+export default Scroll;  
